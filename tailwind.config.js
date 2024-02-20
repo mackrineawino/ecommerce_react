c9802d4f77df9e090a08f8fc7bb1cb08337ec830
mackrineawino <awinomackrine1@gmail.com>
2024-02-20 11:26:58 +0300

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
-    screens: {
-      sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
-      md: "768px",
-      // => @media (min-width: 1024px) { ... }
-
-      lg: "1280px",
-      // => @media (min-width: 1280px) { ... }
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
-};
-
+}
